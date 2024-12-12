37edc6f778a654d702c0809afcb3901c396d0def
Benjamin <yolo45osea@gmail.com>
2024-12-12 18:39:01 -0300

    Movidos todos los archivos a la carpeta mi-carpeta

---
+++ Totem/iniciar.bat
@@ -0,0 +1,4 @@
+pip install django
+python -m pip install Pillow
+pip install qrcode
+py manage.py runserver
--- iniciar.bat
+++
@@ -1,4 +0,0 @@
-pip install django
-python -m pip install Pillow
-pip install qrcode
-py manage.py runserver
